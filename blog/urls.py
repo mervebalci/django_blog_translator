0aaed836eadf374d1840a6f2272603c507342118
mervebalci <mervebalci090@gmail.com>
2022-07-17 14:47:32 -0400

    Listing Blog Posts on the Homepage

--- blog/urls.py
+++ blog/urls.py
@@ -9,17 +9,18 @@ from django.urls import path
 
 urlpatterns = [
     path('<slug:slug>', views.BlogView.as_view(), name='blog_view'),
-    path('about/', views.AboutView.as_view(), name='about_view')
+    path('about/', views.AboutView.as_view(), name='about_view'),
+    path('', views.PostList.as_view(), name='home')    # Empty strings '' represent the homepage
 ]
 # When the user visits a certain URL, let's say example.com/dogs
 # Django will go to DB table and search in each row for dogs.
 # And when it finds the row has dogs, it will execute BlogView as view.
 
 
-# After created index.html, how can user get this html template? What do they do?
+# After created about.html, how can user get this html template? What do they do?
 # They enter a url in their browser. And to make that connection between html and url, a new path will be created (line12)
-# Inside of this new path, HomeView is created as another view and this should be added in views.py as a new class.
-# WHEN USER VISITS THIS '' URL, THIS HOMEVIEW WILL CALL THAT INDEX.HTML TEMPLATE TO BE RENDERED IN THE USER'S BROWSER
+# Inside of this new path, AboutView is created as another view and this should be added in views.py as a new class.
+# WHEN USER VISITS THIS 'about/' URL, THIS AboutVIEW WILL CALL THAT ABOUT.HTML TEMPLATE TO BE RENDERED IN THE USER'S BROWSER
 
 
 # There is another urls.py file inside my_site folder which contains the configurations of the Django project, not the Django apps.
--- blog/views.py
+++ blog/views.py
@@ -9,17 +9,20 @@ from django.shortcuts import render
 from .models import Post
 from django.views import generic
 
+
 # Create your views here.
 
+
+# DetailView to render html templates that gets data from a model
 class BlogView(generic.DetailView):
     model = Post
     template_name = 'blog.html'
-
 # Django expects from this BlogView class to have a model and template_name variables.
 # This model variable should point to the name of the model that should be connected to the blog.
 # The model is Post which is class name from models.py and should be imported (line9).
 
 
+# TemplateView to render html templates without data from a model
 class AboutView(generic.TemplateView):
     template_name = 'about.html'
     # TemplateView means when you only need to render a template without getting data from a model.
@@ -27,3 +30,17 @@ class AboutView(generic.TemplateView):
     # So, this class doesn't need a MODEL. Because about.html is a static page.
     # It's not getting any data from a database like blog.html
     # However, blog.html gets data from database such as title, content, author.
+
+
+# ListView is specialized in rendering multiple data rows/multiple posts
+class PostList(generic.ListView):    # class name has to be in the same format with the one inside for loop in index.html
+    queryset = Post.objects.filter(status=1).order_by('date_created')
+    template_name = 'index.html'
+    # This type of class expects a queryset variable which should be equal to Post.
+    # Post is the model class in models.py
+    # After that, query the objects and then add a filter. So, it will only show the posts that are published.
+    # And to put them in an order maybe according to date created, use 'order_by'
+    # With this version, webpage will always show the oldest post on the top and keeps adding the new ones at the end.
+    # If you want to change it to show the newest one always on top, then you need to write like this .order_by('-date_created')
+    
+    # AFTER THE NEW VIEW IS CREATED, THE NEW PATH SHOULD BE ADDED IN URLS.PY
