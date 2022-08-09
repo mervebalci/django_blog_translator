7b6ee83d7dc4f8425a20be4365e17fd458eed9c1
mervebalci <mervebalci090@gmail.com>
2022-08-08 22:34:46 -0400

    Getting and Processing User Input Through a Form

--- translator/views.py
+++ translator/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def translator_view(request):
-    return render(request, 'translator.html')
+    if request.method == 'POST':
+        input_text = request.POST['my_textarea']
+        output = input_text.upper()
+        return render(request, 'translator.html', {'output_text':output, 'input_text':input_text})
+    else:
+        return render(request, 'translator.html')
 
 # This gets a request as parameter. So this function on the background it expects a request.
 # And the URL here, will pass that request object when the user visits the website.
